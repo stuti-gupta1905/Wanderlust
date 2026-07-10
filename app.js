62f299aa6b90ff6c30e77436139b4e20b2c63ce7
Stuti <stutigupta01905@gmail.com>
2026-07-10 13:55:03 +0530

    Add root route

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV != "production") {
 
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
